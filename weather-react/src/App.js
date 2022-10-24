9a5c784e3cab646ac8fea097cb3a726544e303b3
kieul <kieu.thuy.linh1@gmail.com>
2022-10-24 19:10:42 +0200

    New project
    Working on my portfolio

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,87 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <form className="searchBar" id="search-form">
-          <div className="row">
-            <div className="col-8">
-              <input
-                type="search"
-                placeholder="Enter a city..."
-                class="form-control"
-                id="city-input"
-                autocomplete="off"
-              />
-            </div>
-            <div className="col-4">
-              <input type="submit" value="Search" className="submit-btn" />
-              <button
-                type="submit"
-                id="current-location"
-                className="current-location-btn"
-                value="Current Location"
-              >
-                📍
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-      <br />
-      <div className="middle">
-        <div className="row">
-          <div className="col-6">
-            <div className="float-left">
-              <span className="degreelarge" id="temperature">
-                24
-              </span>
-              <span className="units">
-                <a href="#" id="celsius-link">
-                  °C
-                </a>{" "}
-                |
-                <a href="#" id="fahrenheit-link">
-                  °F
-                </a>
-              </span>
-            </div>
-          </div>
-          <div className="col-6">
-            <h2 className="city" id="displayed-city"></h2>
-            <h4 id="current-time"></h4>
-            <h3 id="description">Mostly sunny</h3>
-            <span id="maxTemperature"></span>
-            <span id="maxTemperature">°C</span> |
-            <span id="minTemperature"></span>
-            <span id="maxTemperature">°C</span>
-          </div>
-        </div>
-      </div>
-      <br />
-      <div className="maincard" id="forecast"></div>
-      <br />
-      <small id="creator">
-        <a
-          href="https://github.com/kieul/Weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        <span> and design by </span>
-        <a
-          href="https://www.linkedin.com/in/linh-kieu/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Linh Kieu
-        </a>
-      </small>
+      <Weather />
     </div>
   );
 }
-
-export default App;
